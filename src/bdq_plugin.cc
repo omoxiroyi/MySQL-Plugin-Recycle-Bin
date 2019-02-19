58eb5451b860d1530420cfedbc546373a14f17f2
sunashe <18079612186@163.com>
2019-02-19 20:44:20 +0800

    基本实现recycle_bin purge功能

--- src/bdq_plugin.cc
+++ src/bdq_plugin.cc
@@ -23,43 +23,46 @@
 #include <auth/sql_acl.h>
 #include <sql_tablespace.h>
 #include <sql_base.h>
+#include <sql_handler.h>
 #include "log_event.h"
 #include "sql_parse.h"
 #include "conn_handler/channel_info.h"
 #include "sql_rename.h"
 #include "sql_db.h"
 #include "sql_table.h"
+#include "bdq_purger.h"
+#include "mysys_err.h"
+#include "my_byteorder.h"
+
+#ifdef HAVE_PSI_INTERFACE
+PSI_mutex_key  key_ss_mutex_bdq_purge_mutex;
+PSI_cond_key key_ss_cond_bdq_purge_cond;
+PSI_thread_key key_ss_thread_bdq_purge_thread;
+#endif /* HAVE_PSI_INTERFACE */
+
+
+
+
 
 
 void switch_recycle_bin_status(char in_status);
+bool purged_table();
 
 
 bdqSlave bdq_slave;
 static Format_description_log_event*  glob_description_event = NULL;
 static my_bool opt_verify_binlog_checksum= TRUE;
 static my_bool received_fde = FALSE;
-static THD* bdq_backup_thd = NULL;
+THD* bdq_backup_thd = NULL;
 static char query_create_table[]="CREATE TABLE  (id int not null auto_increment primary key)ENGINE=INNODB";
-static const char query_create_backup_database[] = "create database if not exists recycle_bin_bdq";
-static const int iso8601_size= 33;
+static const char query_create_backup_database[] = "create database if not exists ``";
+const int iso8601_size= 33;
+static const char recycle_bin_time_flag[] = "ashesun";
 
-
-
-/**
-  Make and return an ISO 8601 / RFC 3339 compliant timestamp.
-  Heeds log_timestamps.
-
-  @param buf       A buffer of at least 26 bytes to store the timestamp in
-                   (19 + tzinfo tail + \0)
-  @param seconds   Seconds since the epoch, or 0 for "now"
-
-  @return          length of timestamp (excluding \0)
-*/
-
-static int make_recycle_bin_iso8601_timestamp(char *buf, ulonglong utime = 0)
+ulonglong make_recycle_bin_iso8601_timestamp(char *buf, ulonglong utime = 0)
 {
   struct tm  my_tm;
-  char       tzinfo[7]="Z";  // max 6 chars plus \0
+//  char       tzinfo[7]="Z";  // max 6 chars plus \0
   size_t     len;
   time_t     seconds;
 
@@ -75,37 +78,39 @@ static int make_recycle_bin_iso8601_timestamp(char *buf, ulonglong utime = 0)
   {
     localtime_r(&seconds, &my_tm);
 
-#ifdef __FreeBSD__
-    /*
-      The field tm_gmtoff is the offset (in seconds) of the time represented
-      from UTC, with positive values indicating east of the Prime Meridian.
-    */
-    long tim= -my_tm.tm_gmtoff;
-#elif _WIN32
-    long tim = _timezone;
-#else
-    long tim= timezone; // seconds West of UTC.
-#endif
-    char dir= '-';
-
-    if (tim < 0)
-    {
-      dir= '+';
-      tim= -tim;
-    }
-    my_snprintf(tzinfo, sizeof(tzinfo), "%c%02d:%02d",
-                dir, (int) (tim / (60 * 60)), (int) ((tim / 60) % 60));
+//#ifdef __FreeBSD__
+//    /*
+//      The field tm_gmtoff is the offset (in seconds) of the time represented
+//      from UTC, with positive values indicating east of the Prime Meridian.
+//    */
+//    long tim= -my_tm.tm_gmtoff;
+//#elif _WIN32
+//    long tim = _timezone;
+//#else
+//    long tim= timezone; // seconds West of UTC.
+//#endif
+//    char dir= '-';
+//
+//    if (tim < 0)
+//    {
+//      dir= '+';
+//      tim= -tim;
+//    }
+//    my_snprintf(tzinfo, sizeof(tzinfo), "%c%02d:%02d",
+//                dir, (int) (tim / (60 * 60)), (int) ((tim / 60) % 60));
   }
 
-  len= my_snprintf(buf, iso8601_size, "%04d%02d%02d%02d%02d%02d",
-                   my_tm.tm_year + 1900,
-                   my_tm.tm_mon  + 1,
-                   my_tm.tm_mday,
-                   my_tm.tm_hour,
-                   my_tm.tm_min,
-                   my_tm.tm_sec);
+//  len= my_snprintf(buf, iso8601_size, "%04d%02d%02d%02d%02d%02d",
+//                   my_tm.tm_year + 1900,
+//                   my_tm.tm_mon  + 1,
+//                   my_tm.tm_mday,
+//                   my_tm.tm_hour,
+//                   my_tm.tm_min,
+//                   my_tm.tm_sec);
+  utime = seconds*1000000+utime;
+  len = my_snprintf(buf,iso8601_size,"%lu",utime);
 
-  return std::min<int>(len, iso8601_size - 1);
+  return utime;
 }
 
 /**
@@ -166,10 +171,15 @@ void bdq_after_execute_command(THD* bdq_backup_thd)
 */
 my_bool bdq_backup_table_routine(const char* table_dropped_name,const char* db,const char* backup_dir,THD* bdq_backup_thd)
 {
-  char* query_rename_table = new char[strlen("RENAME TABLE %s.%s to recycle_bin_bdq.%s_%s")+
-                                      (strlen(db)+strlen(table_dropped_name))*2 + iso8601_size];
+  char* query_rename_table = new char[strlen("RENAME TABLE %s.%s to %s.%s_%s")+ strlen(recycle_bin_database_name)+
+                                      (strlen(db)+strlen(table_dropped_name))*2 + iso8601_size +
+                                      strlen(recycle_bin_time_flag)];
+
   char* query_create_virtual_table = new char[strlen(query_create_table) + strlen(db) + strlen(table_dropped_name)];
+
+  char* query_create_recycle_bin_db = new char[strlen(query_create_backup_database)+strlen(recycle_bin_database_name)];
   int res = FALSE;
+  my_bool backup_complete = FALSE;
   char my_timestamp[iso8601_size];
 
   make_recycle_bin_iso8601_timestamp(my_timestamp);
@@ -181,8 +191,9 @@ my_bool bdq_backup_table_routine(const char* table_dropped_name,const char* db,c
     bdq_backup_thd->variables.option_bits &= ~OPTION_BIN_LOG;
 
   //stage 2.create database if not exists.
+  sprintf(query_create_recycle_bin_db,"create database if not exists %s",recycle_bin_database_name);
   if(bdq_prepare_execute_command(bdq_backup_thd,
-                                 query_create_backup_database,db,table_dropped_name))
+                                 query_create_recycle_bin_db,db,table_dropped_name))
   {
     LEX  *const lex= bdq_backup_thd->lex;
     HA_CREATE_INFO create_info(lex->create_info);
@@ -199,18 +210,27 @@ my_bool bdq_backup_table_routine(const char* table_dropped_name,const char* db,c
     if(res)
     {
       sql_print_error("Create backup database error");
+      backup_complete = FALSE;
       goto exit_bdq_btr;
     }
   }
   else
   {
+    backup_complete = FALSE;
     goto exit_bdq_btr;
   }
 
-  //stage 3.rename A.a to B.a.back.timestamp
-  sprintf(query_rename_table,"RENAME TABLE `%s`.`%s` to `recycle_bin_bdq`.`%s_%s_%s`",
+  //stage 3.rename A.a for backup.
+  sprintf(query_rename_table,"RENAME TABLE `%s`.`%s` to `%s`.`%s_%s_%s_%s`",
           db,table_dropped_name,
-          db,table_dropped_name,my_timestamp);
+          recycle_bin_database_name,db,table_dropped_name,recycle_bin_time_flag,my_timestamp);
+
+  if((strlen(db)+strlen(table_dropped_name)+strlen(recycle_bin_time_flag)+strlen(my_timestamp)) > 64)
+  {
+    backup_complete = FALSE;
+    sql_print_error("Backup table name is too long,%s",query_rename_table);
+    goto exit_bdq_btr;
+  }
   if(bdq_prepare_execute_command(bdq_backup_thd,query_rename_table,db,table_dropped_name))
   {
     LEX  *const lex= bdq_backup_thd->lex;
@@ -222,21 +242,24 @@ my_bool bdq_backup_table_routine(const char* table_dropped_name,const char* db,c
     {
       bdq_after_execute_command(bdq_backup_thd);
       sql_print_error("Backup table in rename stage failed");
+      backup_complete = FALSE;
       goto exit_bdq_btr;
     }
     bdq_after_execute_command(bdq_backup_thd);
   }
   else
   {
+    backup_complete = FALSE;
     goto exit_bdq_btr;
   }
 
+  //在数据库层执行完rename之后就是已经完成备份操作了，但是如果stage 4出错的话，可能会影响复制的SQL线程正常回放。
+  backup_complete = TRUE;
   //stage 4.create A.a(id int not null auto_increment primary key);
   sprintf(query_create_virtual_table,
           "CREATE TABLE %s.%s(id int not null auto_increment primary key)ENGINE=INNODB",db,table_dropped_name);
   if(bdq_prepare_execute_command(bdq_backup_thd,query_create_virtual_table,db,table_dropped_name))
   {
-    res= FALSE;
     LEX  *const lex= bdq_backup_thd->lex;
     /* first SELECT_LEX (have special meaning for many of non-SELECTcommands) */
     SELECT_LEX *const select_lex= lex->select_lex;
@@ -293,8 +316,9 @@ my_bool bdq_backup_table_routine(const char* table_dropped_name,const char* db,c
   bdq_after_execute_command(bdq_backup_thd);
   delete[] query_rename_table;
   delete[] query_create_virtual_table;
+  delete[] query_create_recycle_bin_db;
 
-  return TRUE;
+  return backup_complete;
 }
 
 
@@ -307,7 +331,6 @@ my_bool bdq_backup_table_routine(const char* table_dropped_name,const char* db,c
 */
 my_bool bdq_backup(const char* drop_query,const char* db,const char* backup_dir)
 {
-  bdq_backup_thd = (THD*)my_get_thread_local(THR_THD);
   Parser_state parser_state;
   struct st_mysql_const_lex_string new_db;
   new_db.str=db;
@@ -370,8 +393,6 @@ my_bool bdq_backup(const char* drop_query,const char* db,const char* backup_dir)
       break;
     }
   }
-
-
   return TRUE;
 }
 
@@ -389,7 +410,6 @@ int bdq_request_dump(Binlog_relay_IO_param *param,
 {
   return 0;
 }
-
 
 
 int bdq_read_event(Binlog_relay_IO_param *param,
@@ -414,6 +434,11 @@ int bdq_read_event(Binlog_relay_IO_param *param,
                                           &tmp_event_buf, &tmp_event_len);
 
   type = (Log_event_type)tmp_event_buf[EVENT_TYPE_OFFSET];
+
+  if(type == binary_log::HEARTBEAT_LOG_EVENT)
+  {
+    purged_table();
+  }
 
   if(!received_fde) //还没有收到过FORMAT_DESCRIPTION_EVENT，进行检测。
   {
@@ -498,6 +523,7 @@ int bdq_queue_event(Binlog_relay_IO_param *param,
 int bdq_io_start(Binlog_relay_IO_param *param)
 {
   //Nothing to do.
+  bdq_backup_thd = (THD*)my_get_thread_local(THR_THD);
   return 0;
 }
 
@@ -576,18 +602,28 @@ static MYSQL_SYSVAR_ULONG(trace_level, recycle_bin_trace_level,
                           &fix_recycle_bin_trace_level, // update
   32, 0, ~0UL, 1);
 
-static MYSQL_SYSVAR_ULONG(expire_hours,
-        recycle_bin_expire_hours,
+static MYSQL_SYSVAR_ULONG(expire_seconds,
+        recycle_bin_expire_seconds,
         PLUGIN_VAR_OPCMDARG,
         "recycle bin expire hours",
         NULL,
         NULL,
-        2,0,48,1);
+        1800,0,172800,1);
+
+static MYSQL_SYSVAR_STR(database_name,
+        recycle_bin_database_name,
+                        PLUGIN_VAR_OPCMDARG,
+                        "recycle bin database name",
+                        NULL,
+                        NULL,
+                        "recycle_bin_dba"
+        );
 
 static SYS_VAR* bdq_system_vars[]= {
   MYSQL_SYSVAR(enabled),
   MYSQL_SYSVAR(trace_level),
-  MYSQL_SYSVAR(expire_hours),
+  MYSQL_SYSVAR(expire_seconds),
+  MYSQL_SYSVAR(database_name),
   NULL,
 };
 
@@ -619,7 +655,6 @@ static int bdq_plugin_init(void *p)
   {
     return 1;
   }
-
   glob_description_event= new Format_description_log_event(3);
   return 0;
 }
@@ -660,3 +695,260 @@ mysql_declare_plugin(bdq)
   0,                            /* flags */
 }
 mysql_declare_plugin_end;
+
+
+long mysql_rm_arc_files(THD *thd, MY_DIR *dirp, const char *org_path);
+
+const char *del_exts[]= {".frm", ".BAK", ".TMD", ".opt", ".OLD", ".cfg", NullS};
+static TYPELIB deletable_extentions=
+        {array_elements(del_exts)-1,"del_exts", del_exts, NULL};
+
+static bool find_db_tables_should_be_purged(THD *thd, MY_DIR *dirp,
+                                              const char *db,
+                                              const char *path,
+                                              TABLE_LIST **tables,
+                                              bool *found_other_files,ulonglong utime)
+{
+  char filePath[FN_REFLEN];
+  TABLE_LIST *tot_list=0, **tot_list_next_local, **tot_list_next_global;
+  DBUG_ENTER("find_db_tables_and_rm_known_files");
+  DBUG_PRINT("enter",("path: %s", path));
+  TYPELIB *known_extensions= ha_known_exts();
+  ulonglong table_backup_time = 0;
+
+  tot_list_next_local= tot_list_next_global= &tot_list;
+
+  for (uint idx=0 ;
+       idx < dirp->number_off_files && !thd->killed ;
+       idx++)
+  {
+    table_backup_time = 0;
+    FILEINFO *file=dirp->dir_entry+idx;
+    char *extension;
+    DBUG_PRINT("info",("Examining: %s", file->name));
+
+    /* skiping . and .. */
+    if (file->name[0] == '.' && (!file->name[1] ||
+                                 (file->name[1] == '.' &&  !file->name[2])))
+      continue;
+
+    if (file->name[0] == 'a' && file->name[1] == 'r' &&
+        file->name[2] == 'c' && file->name[3] == '\0')
+    {
+      /* .frm archive:
+        Those archives are obsolete, but following code should
+        exist to remove existent "arc" directories.
+      */
+      char newpath[FN_REFLEN];
+      MY_DIR *new_dirp;
+      strxmov(newpath, path, "/", "arc", NullS);
+      (void) unpack_filename(newpath, newpath);
+      if ((new_dirp = my_dir(newpath, MYF(MY_DONT_SORT))))
+      {
+        DBUG_PRINT("my",("Archive subdir found: %s", newpath));
+        if ((mysql_rm_arc_files(thd, new_dirp, newpath)) < 0)
+          DBUG_RETURN(true);
+        continue;
+      }
+      *found_other_files= true;
+      continue;
+    }
+    if (!(extension= strrchr(file->name, '.')))
+      extension= strend(file->name);
+    if (find_type(extension, &deletable_extentions, FIND_TYPE_NO_PREFIX) <= 0)
+    {
+      if (find_type(extension, known_extensions, FIND_TYPE_NO_PREFIX) <= 0)
+        *found_other_files= true;
+      continue;
+    }
+    /* just for safety we use files_charset_info */
+    if (db && !my_strcasecmp(files_charset_info,
+                             extension, reg_ext))
+    {
+      /* Drop the table nicely */
+      *extension= 0;			// Remove extension
+      TABLE_LIST *table_list=(TABLE_LIST*)
+              thd->mem_calloc(sizeof(*table_list) +
+                              strlen(db) + 1 +
+                              MYSQL50_TABLE_NAME_PREFIX_LENGTH +
+                              strlen(file->name) + 1);
+
+      if (!table_list)
+        DBUG_RETURN(true);
+      table_list->db= (char*) (table_list+1);
+      table_list->db_length= my_stpcpy(const_cast<char*>(table_list->db),
+                                       db) - table_list->db;
+      table_list->table_name= table_list->db + table_list->db_length + 1;
+      table_list->table_name_length= filename_to_tablename(file->name,
+                                                           const_cast<char*>(table_list->table_name),
+                                                           MYSQL50_TABLE_NAME_PREFIX_LENGTH +
+                                                           strlen(file->name) + 1);
+      table_list->open_type= OT_BASE_ONLY;
+
+      /* To be able to correctly look up the table in the table cache. */
+      if (lower_case_table_names)
+        table_list->table_name_length= my_casedn_str(files_charset_info,
+                                                     const_cast<char*>(table_list->table_name));
+
+      table_list->alias= table_list->table_name;	// If lower_case_table_names=2
+      table_list->internal_tmp_table= is_prefix(file->name, tmp_file_prefix);
+      MDL_REQUEST_INIT(&table_list->mdl_request,
+                       MDL_key::TABLE, table_list->db,
+                       table_list->table_name, MDL_EXCLUSIVE,
+                       MDL_TRANSACTION);
+
+      int time_buf_start_pos =0;
+      for(;time_buf_start_pos < table_list->table_name_length;time_buf_start_pos++)
+      {
+        if(strncasecmp(table_list->table_name+time_buf_start_pos,
+                recycle_bin_time_flag,sizeof(recycle_bin_time_flag)-1) ==0 )
+        {
+          time_buf_start_pos +=strlen(recycle_bin_time_flag);
+          break;
+        }
+      }
+
+      if(time_buf_start_pos == table_list->table_name_length)
+      {
+        continue;
+      }
+
+      //int8store((char*)(table_list->table_name+time_buf_start_pos+1),table_backup_time);
+      table_backup_time = atoll(table_list->table_name + time_buf_start_pos+1);
+
+      if(table_backup_time > utime) //未到purge 时间
+      {
+        continue;
+      }
+      /* Link into list */
+      (*tot_list_next_local)= table_list;
+      (*tot_list_next_global)= table_list;
+      tot_list_next_local= &table_list->next_local;
+      tot_list_next_global= &table_list->next_global;
+    }
+    else
+    {
+//      strxmov(filePath, path, "/", file->name, NullS);
+//      /*
+//        We ignore ENOENT error in order to skip files that was deleted
+//        by concurrently running statement like REAPIR TABLE ...
+//      */
+//      if (my_delete_with_symlink(filePath, MYF(0)) &&
+//          my_errno() != ENOENT)
+//      {
+//        char errbuf[MYSYS_STRERROR_SIZE];
+//        my_error(EE_DELETE, MYF(0), filePath,
+//                 my_errno(), my_strerror(errbuf, sizeof(errbuf), my_errno()));
+//        DBUG_RETURN(true);
+//      }
+    }
+  }
+  *tables= tot_list;
+  DBUG_RETURN(false);
+}
+
+bool purge_tables_before_time(THD* thd,st_mysql_const_lex_string db,bool if_exists,bool silent,ulonglong utime)
+{
+  Drop_table_error_handler err_handler;
+  ulong deleted_tables= 0;
+  bool error= true;
+  char	path[2 * FN_REFLEN + 16];
+  MY_DIR *dirp;
+  size_t length;
+  bool found_other_files= false;
+  TABLE_LIST *tables= NULL;
+  TABLE_LIST *table;
+  //Drop_table_error_handler err_handler;
+  DBUG_ENTER("mysql_rm_db");
+
+
+  if (lock_schema_name(thd, db.str))
+    DBUG_RETURN(true);
+
+  length= build_table_filename(path, sizeof(path) - 1, db.str, "", "", 0);
+  my_stpcpy(path+length, MY_DB_OPT_FILE);		// Append db option file name
+  // del_dbopt(path);				// Remove dboption hash entry
+  path[length]= '\0';				// Remove file name
+
+  /* See if the directory exists */
+  if (!(dirp= my_dir(path,MYF(MY_DONT_SORT))))
+  {
+    if (!if_exists)
+    {
+      my_error(ER_DB_DROP_EXISTS, MYF(0), db.str);
+      DBUG_RETURN(true);
+    }
+    else
+    {
+      push_warning_printf(thd, Sql_condition::SL_NOTE,
+                          ER_DB_DROP_EXISTS, ER(ER_DB_DROP_EXISTS), db.str);
+      error= false;
+    }
+  }
+
+  if ((error = find_db_tables_should_be_purged(thd, dirp, db.str, path, &tables,
+                                        &found_other_files,utime)) )
+  {
+    goto exit;
+  }
+
+  if(!tables)
+  {
+    goto exit;
+  }
+
+  if ((error=lock_table_names(thd, tables, NULL, thd->variables.lock_wait_timeout, 0)))
+  {
+    goto exit;
+  }
+
+  if(tables)
+  {
+    mysql_ha_rm_tables(thd, tables);
+  }
+
+  for (table= tables; table; table= table->next_local)
+  {
+    tdc_remove_table(thd, TDC_RT_REMOVE_ALL, table->db, table->table_name,
+                     false);
+    deleted_tables++;
+  }
+  thd->push_internal_handler(&err_handler);
+  if (!thd->killed &&
+      !(tables &&
+        mysql_rm_table_no_locks(thd, tables, true, false, true, true)))
+  {
+    //nothing to do.
+  }
+  thd->pop_internal_handler();
+
+  exit:
+  /*
+    If this database was the client's selected database, we silently
+    change the client's selected database to nothing (to have an empty
+    SELECT DATABASE() in the future). For this we free() thd->db and set
+    it to 0.
+  */
+
+ // bdq_after_execute_command(thd);
+
+  bdq_backup_thd->mdl_context.release_statement_locks();
+  bdq_backup_thd->mdl_context.release_transactional_locks();
+
+  DBUG_RETURN(error);
+}
+
+bool purged_table()
+{
+  char now_timestamp[iso8601_size];
+  ulonglong now_utime = make_recycle_bin_iso8601_timestamp(now_timestamp);
+  ulonglong table_should_be_purged_time = now_utime - recycle_bin_expire_seconds*1000000;
+  st_mysql_const_lex_string db_string;
+  db_string.str = recycle_bin_database_name;
+  db_string.length = strlen(recycle_bin_database_name);
+
+  if(purge_tables_before_time(bdq_backup_thd,db_string,true,true,table_should_be_purged_time))
+  {
+    sql_print_error("purged tables failed");
+  }
+}
